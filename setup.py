13e6108326bace965553a8bf5993d01478197b08
Jonathan Morton <jonathan.morton.ctr@usgs.gov>
2016-09-28 14:56:30 -0500

    Begin defining most basic tests

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ setup(
     # $ pip install -e .[dev,test]
     extras_require={
         'dev': [],
-        'test': ['coverage>=4.2', 'pytest>=3.0.2'],
+        'test': ['coverage>=4.2', 'pytest>=3.0.2', 'aniso8601>=1.1.0'],
     },
 
     setup_requires=['pytest-runner', 'pip'],
--- test/test_ccd.py
+++ test/test_ccd.py
@@ -1,2 +1,76 @@
-def test1and2():
-    assert 2 > 1
+import aniso8601
+import datetime
+import numpy as np
+
+from ccd.models import lasso
+from itertools import cycle, islice
+
+
+
+# Test data generators for change detection.
+
+def gen_acquisition_dates(interval):
+    """Generate acquisition dates for an ISO8601 interval.
+
+    Args:
+        interval: An ISO8610 interval.
+
+    Returns:
+        generator producing datetime.date or datetime.datetime
+        depending on the resolution of the interval.
+
+    Example:
+        gen_acquisition_dates('R10/2015-01-01/P16D')
+    """
+    dates = aniso8601.parse_repeating_interval(interval)
+    return dates
+
+def gen_acquisition_delta(interval):
+    """Generate delta in days for an acquisition day since 1970-01-01.
+
+    Args:
+        interval: ISO8601 date range
+
+    Returns:
+
+    Example:
+
+    """
+    epoch = datetime.datetime.utcfromtimestamp(0).date()
+    dates = gen_acquisition_dates(interval)
+    yield [(date-epoch).days for date in dates]
+
+def acquisition_delta(interval):
+    """ List of delta in days for an interval """
+    return list(*gen_acquisition_delta(interval))
+
+def repeated_values(samples, seed=42):
+    np.random.seed(seed)
+    sine = np.array(list(islice(cycle([0, 1, 0, -1]), None, samples)))
+    noise = np.array(np.random.random(samples))
+    return sine+noise
+
+
+
+def test_not_enough_observations():
+    acquired = acquisition_delta('R15/P16D/2000-01-01')
+    observes = repeated_values(15)
+    assert len(acquired) == len(observes) == 15
+
+def test_enough_observations():
+    acquired = acquisition_delta('R16/P16D/2000-01-01')
+    observes = repeated_values(16)
+    assert len(acquired) == len(observes) == 16
+
+def test_two_changes_during_time():
+    acquired = acquisition_delta('R50/P16D/2000-01-01')
+    observes = np.hstack((repeated_values(25) + 10,
+                          repeated_values(25) + 50))
+    assert len(acquired) == len(observes) == 50
+
+def test_three_changes_during_time():
+    acquired = acquisition_delta('R90/P16D/2000-01-01')
+    observes = np.hstack((repeated_values(30) + 10,
+                          repeated_values(30) + 50,
+                          repeated_values(30) + 10))
+    assert len(acquired) == len(observes) == 90
