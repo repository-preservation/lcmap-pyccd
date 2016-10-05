988257d87b3e702bda7d196ba0cfdd35faac3eaf
Jonathan Morton <jmorton@element84.com>
2016-10-05 12:25:28 -0500

    Fix refence to sample data, update shape test

--- test/test_filter.py
+++ test/test_filter.py
@@ -1,21 +1,11 @@
 import shared
 
 def test_basic_loading_sample():
-    data = shared.read_data("resources/sample_1.csv")
-    assert data.shape == (22,8)
+    data = shared.read_data("test/resources/sample_1.csv")
+    assert data.shape == (69,9)
 
 
 def test_filtering_using_no_data():
-    data = shared.read_data("resources/sample_1.csv")
+    data = shared.read_data("test/resources/sample_1.csv")
     masker = data[:,1] != 20000
-    assert data[masker,:].shape == (17,8)
-
-
-def test_filtering_cloudy_data_using_masks():
-    data = shared.read_data("resources/sample_1.csv")
-    assert False, "TODO"
-
-
-def test_permanent_snow_detection_using_masks():
-    data = shared.read_data("resources/sample_1.csv")
-    assert False, "TODO"
+    assert data[masker,:].shape == (66,9)
