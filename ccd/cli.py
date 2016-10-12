9fdbefc970bcb4b03c5d4d3495497450032c7fe7
Jonathan Morton <jmorton@element84.com>
2016-10-12 12:55:34 -0500

    Format results as JSON or text table

--- ccd/cli.py
+++ ccd/cli.py
@@ -6,7 +6,9 @@ import ccd
 from click_plugins import with_plugins
 from pkg_resources import iter_entry_points
 import click
+import json
 import numpy as np
+
 
 logger = app.logging.getLogger(__name__)
 
@@ -20,23 +22,48 @@ def cli():
 
 @cli.command()
 @click.argument('path')
-def sample(path):
+@click.option('--format', default='json', type=click.Choice(['json','table']))
+def sample(path, format):
     """Subcommand for processing sample data."""
 
-    logger.info("Loading data...")
+    logger.debug("Loading data...")
     samples = np.genfromtxt(path, delimiter=',', dtype=np.int).T
 
-    logger.info("Building change model...")
+    logger.debug("Building change model...")
     results = ccd.detect(*samples)
 
-    logger.info("Done...")
-    click.echo(results)
+    if format == 'table':
+        click.echo(results_to_table(results))
+    else:
+        click.echo(json.dumps(results, indent=2))
 
+    logger.debug("Done...")
 
 @cli.command()
 def another_subcommand():
     """Another Subcommand that does something."""
     logger.info("Another Subcommand running...")
+
+def results_to_table(results):
+    """Moo."""
+    change_format = "Time Segment {0}: {1}...{2}"
+    band_format = "{0:10} {1:10.4f} {2:10.4f} {3:10.4} {4:10.4f} {5:10.4f} {6:>10.4f} {7:>15.2f}"
+    columns = ["band", "mags", 'rmse', 'c1', 'c2', 'c3', 'c4', 'intercept']
+    for ix, segment in enumerate(results):
+
+        # describe the segment period and inputs
+        click.echo(change_format.format(ix, segment['start_day'], segment['end_day']))
+
+        click.echo("{0:<10} {1:>10} {2:>10} {3:>10} {4:>10} {5:>10} {6:>10} {7:>15}".format(*columns))
+        for color in ['red','green','blue','nir','swir1','swir2']:
+            click.echo(band_format.format(*[color,
+                                          segment[color]['magnitude'],
+                                          segment[color]['rmse'],
+                                          segment[color]['coefficients'][0],
+                                          segment[color]['coefficients'][1],
+                                          segment[color]['coefficients'][2],
+                                          segment[color]['coefficients'][3],
+                                          segment[color]['intercept']]))
 
 if __name__ == '__main__':
     cli()
