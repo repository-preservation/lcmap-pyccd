34a6e6035c609bb4d566e29ba0eb13ed24ac6b82
David Hill <davidvhill@users.noreply.github.com>
2016-10-18 14:07:40 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/davidvhill/pyccd',
+    url='https://github.com/usgs-eros/lcmap-pyccd',
 
     # Author details
     author='',
