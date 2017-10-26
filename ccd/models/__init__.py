7516fe540a7c1b3ebaa1feb01eadbcff2e8fd75f
klsmith-usgs <klsmith@usgs.gov>
2017-10-26 10:51:27 -0500

    Change all output to standard python types

--- ccd/models/__init__.py
+++ ccd/models/__init__.py
@@ -17,6 +17,9 @@ def results_to_changemodel(fitted_models, start_day, end_day, break_day,
     Helper method to consolidate results into a concise, self documenting data
     structure.
 
+    This also converts any specific package types used during processing to
+    standard python types to help with downstream processing.
+
     {start_day: int,
      end_day: int,
      break_day: int,
@@ -35,18 +38,19 @@ def results_to_changemodel(fitted_models, start_day, end_day, break_day,
     """
     spectral_models = []
     for ix, model in enumerate(fitted_models):
-        spectral = {'rmse': model.rmse,
-                    'coefficients': model.fitted_model.coef_,
-                    'intercept': model.fitted_model.intercept_,
-                    'magnitude': magnitudes[ix]}
+        spectral = {'rmse': float(model.rmse),
+                    'coefficients': tuple(float(c) for c in
+                                          model.fitted_model.coef_),
+                    'intercept': float(model.fitted_model.intercept_),
+                    'magnitude': float(magnitudes[ix])}
         spectral_models.append(spectral)
 
-    return {'start_day': start_day,
-            'end_day': end_day,
-            'break_day': break_day,
-            'observation_count': observation_count,
-            'change_probability': change_probability,
-            'curve_qa': curve_qa,
+    return {'start_day': int(start_day),
+            'end_day': int(end_day),
+            'break_day': int(break_day),
+            'observation_count': int(observation_count),
+            'change_probability': float(change_probability),
+            'curve_qa': int(curve_qa),
             'blue': spectral_models[0],
             'green': spectral_models[1],
             'red': spectral_models[2],
