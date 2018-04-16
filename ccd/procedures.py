71398f655581b0dc1a0a1d5f45bee6b2746cf024
htollerud-usgs <35779267+htollerud-usgs@users.noreply.github.com>
2018-04-16 17:37:14 -0500

    Update procedures.py

--- ccd/procedures.py
+++ ccd/procedures.py
@@ -636,7 +636,7 @@ def lookforward(dates, observations, model_window, fitter_fn, processing_mask,
             spectral_obs = observations[:, processing_mask]
             X = allTimeX[processing_mask,:]
 
-            if model_window.stop + peek_size >= period.shape[0]:
+            if model_window.stop + peek_size > period.shape[0]:
                 break
 
             continue
